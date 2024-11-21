108f3b0f751cf513644c0b3036b2968989ccd7c7
Shashwat <3dartist.shashwat@gmail.com>
2024-11-21 14:23:09 +0530

    Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,21 +11,45 @@ public class PlayerController : MonoBehaviour
     public Vector2 crouchingColliderSize = new Vector2(0.9461729f, 1.321176f); 
     public Vector2 standingColliderOffset = new Vector2(0.006101638f, 0.9829593f);
     public Vector2 crouchingColliderOffset = new Vector2(-0.1175911f, 0.5880105f);
-
+    public float speed;
+    private Rigidbody2D rb2d;
+    public float jump;
     private void Awake()
     {
         Debug.Log("Player controller awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-
-        bool jump = Input.GetAxisRaw("Vertical") > 0;
-        animator.SetBool("Jump", jump);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        PlayerMovementAnimation(horizontal, vertical);
+        MoveCharacter(horizontal, vertical);
+        
+        
+       
+       // bool jump = Input.GetAxisRaw("Vertical") > 0;
+        //animator.SetBool("Jump", jump);
 
         bool crouch = Input.GetKey(KeyCode.LeftControl);
+        CrouchMovement(crouch);
+
+    }
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        if (vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse); 
+        }
+    }
+
+    private void CrouchMovement(bool crouch)
+    {
         animator.SetBool("Crouch", crouch);
 
         if (crouch)
@@ -37,16 +62,30 @@ public class PlayerController : MonoBehaviour
             playerCollider.size = standingColliderSize;
             playerCollider.offset = standingColliderOffset;
         }
+    }
 
+    public void PlayerMovementAnimation(float horizontal, float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //Jump
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
