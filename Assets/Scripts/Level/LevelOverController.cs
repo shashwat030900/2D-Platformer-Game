eac243cda1f056fbe9fb21da9dd5236672dc56cc
Shashwat <3dartist.shashwat@gmail.com>
2024-12-27 13:47:40 +0530

    corrections

--- Assets/Scripts/Level/LevelOverController.cs
+++ Assets/Scripts/Level/LevelOverController.cs
@@ -7,12 +7,20 @@ public class LevelOverController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             Debug.Log("Level finished by the player");
+            if(LevelManager.Instance != null){
+                LevelManager.Instance.MarkCurrentLevelComplete();
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else{
+                Debug.LogError("LevelManager instance is null. Ensure LevelManager is present in the scene.");
+            }
             //SceneManager.LoadScene(2);
            
-            LevelManager.Instance.MarkCurrentLevelComplete();
+           
         }
     }
 }
